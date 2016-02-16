29b5ab4510b8e5a212cec882edc330856c6bc00b
Jennifer Han <jhan2@andrew.cmu.edu>
2016-02-16 09:36:14 -0500

    added the Article model

---
+++ app/models/article.rb
@@ -0,0 +1,15 @@
+class Article < ActiveRecord::Base
+
+	# Relationships
+	belongs_to :category
+
+	# Validations
+  	validates_presence_of :title
+  	validates_presence_of :content
+
+	# Scopes
+	scope :active, where('active = ?', true)
+  	scope :alphabetical, order('title')
+
+
+end
